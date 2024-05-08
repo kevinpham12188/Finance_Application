10df8241ddae98e87dd70de8378514d85d10fbf5
kevinpham7788 <kevinpham7788@gmail.com>
2024-05-08 09:00:04 -0700

    rendering list of search results

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,18 @@ import { CompanySearch } from './company';
 import { searchCompanies } from './api';
 
 function App() {
+  // Store input from users
   const [search, setSearch] = useState<string>("");
+  // Store output from db
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [serverError, setServerError] = useState<string>("");
+  const [serverError, setServerError] = useState<string | null>(null);
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
         console.log(e);
     }
     const onClick = async (e : SyntheticEvent) => {
         const result = await searchCompanies(search);
+        // Type narrowing
         if(typeof result === "string") {
            setServerError(result); 
         } else if(Array.isArray(result.data)) {
@@ -25,7 +28,8 @@ function App() {
   return (
     <div className="App">
       <Search onClick={onClick} search={search} handleChange={handleChange}/>
-      <CardList />
+      {serverError && <div>Unable to connect to API</div>}
+      <CardList searchResults={searchResult} />
     </div>
   );
 }
