58b81370d1b9724ea832283333b326fa5898800a
kevinpham7788 <kevinpham7788@gmail.com>
2024-05-13 15:06:27 -0700

    add sidebar, company profile, income statement components

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
+import { CompanyProfile } from '../../company';
+import { getCompanyProfile } from '../../api';
+import SideBar from '../../Components/SideBar/SideBar';
+import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard';
+import Tile from '../../Components/Tile/Tile';
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
+    let { ticker } = useParams();
+    const [company, setCompany] = useState<CompanyProfile>();
+
+    useEffect(() => {
+        const getProfileInit = async () => {
+            const result = await getCompanyProfile(ticker!);
+            setCompany(result?.data[0]);
+        }
+        getProfileInit()
+    }, [])
   return (
-    <div>CompanyPage</div>
+    <>
+    { company ? (
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+            <SideBar />
+            <CompanyDashboard>
+                <Tile title='Company Name' subTitle={company.companyName}></Tile>
+            </CompanyDashboard>
+      </div>
+    ) : (
+        <div>Company not found!</div>
+    )}
+    </>
   )
 }
 
