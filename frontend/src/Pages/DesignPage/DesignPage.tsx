2d3b02f42fb792bbabeffeb9e8d2323a7719130b
kevinpham7788 <kevinpham7788@gmail.com>
2024-05-14 10:06:12 -0700

    add table and dummy data to test the design

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Table from '../../Components/Table/Table'
+
+type Props = {}
+
+const DesignPage = (props: Props) => {
+  return (
+    <>
+    <h1>Kevin's App Design Page</h1>
+    <h2>
+        This is Kevin's design page. This is where we will house various design aspects of the app
+    </h2>
+    <Table />
+    </>
+  )
+}
+
+export default DesignPage
