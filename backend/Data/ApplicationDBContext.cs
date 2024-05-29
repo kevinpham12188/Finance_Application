3ed172bbfdaa8afcbc9238b2cb5dee70b08fc331
kevinpham7788 <kevinpham7788@gmail.com>
2024-05-28 22:12:56 -0700

    Add stock controller logic

--- backend/Data/ApplicationDBContext.cs
+++ backend/Data/ApplicationDBContext.cs
@@ -11,7 +11,7 @@ namespace backend.Data
             
         }
 
-        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
     }
 }
