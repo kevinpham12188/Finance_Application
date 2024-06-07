e29cea0503429694afb150172356029ec63003aa
kevinpham7788 <kevinpham7788@gmail.com>
2024-06-07 01:14:53 -0700

    add portfolio CRUD logic

---
+++ backend/Interfaces/IPortfolioRepository.cs
@@ -0,0 +1,11 @@
+using backend.Models;
+
+namespace backend.Interfaces
+{
+    public interface IPortfolioRepository
+    {
+         Task<List<Stock>> GetUserPortfolio(AppUser user);
+         Task<Portfolio> CreateAsync(Portfolio portfolio);
+         Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol);
+    }
+}
---
+++ backend/Repository/PortfolioRepository.cs
@@ -0,0 +1,48 @@
+using backend.Data;
+using backend.Interfaces;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repository
+{
+    public class PortfolioRepository : IPortfolioRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public PortfolioRepository(ApplicationDBContext context) {
+            _context = context;
+        }
+
+        public async Task<Portfolio> CreateAsync(Portfolio portfolio)
+        {
+            await _context.portfolios.AddAsync(portfolio);
+            await _context.SaveChangesAsync();
+            return portfolio;
+        }
+
+        public async Task<Portfolio> DeletePortfolio(AppUser appUser, string symbol)
+        {
+            var portfolioModel = await _context.portfolios.FirstOrDefaultAsync(x => x.AppUserId == appUser.Id && x.Stock.Symbol.ToLower() == symbol.ToLower());
+            if (portfolioModel == null) {
+                return null;
+            }
+
+            _context.portfolios.Remove(portfolioModel);
+            await _context.SaveChangesAsync();
+            return portfolioModel;
+        }
+
+        public async Task<List<Stock>> GetUserPortfolio(AppUser user)
+        {
+            return await _context.portfolios.Where(u => u.AppUserId == user.Id)
+            .Select(stock => new Stock{
+                Id = stock.Stock.Id,
+                Symbol = stock.Stock.Symbol,
+                CompanyName = stock.Stock.CompanyName,
+                Purchase = stock.Stock.Purchase,
+                LastDiv = stock.Stock.LastDiv,
+                Industry = stock.Stock.Industry,
+                MarketCap = stock.Stock.MarketCap
+            }).ToListAsync();
+        }
+    }
+}
