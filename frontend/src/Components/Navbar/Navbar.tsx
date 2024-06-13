ed83f462363cc318e557cdbc63a5bc85a812bd78
kevinpham7788 <kevinpham7788@gmail.com>
2024-06-12 23:49:44 -0700

    add wire login and logout logic

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useAuth } from '../../Context/useAuth'
 
 interface Props {}
 
 const Navbar = (props: Props) => {
+  const { isLoggedIn, user, logout } = useAuth();
   return (
     <nav className="relative container mx-auto p-6">
        <div className="flex items-center justify-between">
@@ -17,15 +19,29 @@ const Navbar = (props: Props) => {
             </Link>
            </div>
          </div>
-         <div className="hidden lg:flex items-center space-x-6 text-back">
-           <div className="hover:text-darkBlue">Login</div>
+         { isLoggedIn() ? 
+         (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+           <div className="hover:text-darkBlue">Welcome, {user?.userName}</div>
            <a
-             href=""
+             onClick={logout}
+             className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
+           >
+             Logout
+           </a>
+         </div>
+         ) : 
+         (
+          <div className="hidden lg:flex items-center space-x-6 text-back">
+           <Link to="/login" className="hover:text-darkBlue">Login</Link>
+           <Link
+             to="/register"
              className="px-8 py-3 font-bold rounded text-white bg-lightGreen hover:opacity-70"
            >
              Signup
-           </a>
+           </Link>
          </div>
+         )}
        </div>
      </nav>
   )
